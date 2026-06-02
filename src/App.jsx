9e5ab058d47d27c998a9693807f51938c333cda5
steny-mathew <stenymathew7@gmail.com>
2026-06-02 19:39:06 +0530

    ProblemAtlas MVP Ready
    Completed homepage, atlas visualization,
    opportunity deck, search and filtering,
    and reusable modal architecture.
    Removed routing and simplified structure.

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,7 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
-
 import Home from "./pages/Home";
-import ProblemDetail from "./pages/ProblemDetail";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-
-        <Route
-          path="/"
-          element={<Home />}
-        />
-
-        <Route
-          path="/problem/:slug"
-          element={<ProblemDetail />}
-        />
-
-      </Routes>
-    </BrowserRouter>
-  );
+  return <Home />;
 }
 
 export default App;
